148b935a48b4088c7574b532a8a19ece885ebd4d
apoorwa46 <apoorwa466@gmail.com>
2025-08-31 14:06:34 +0530

    Added CORS config for frontend

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL:import.meta.env.VITE_API_URL || "http://localhost:5000/api" });
+const API = axios.create({ baseURL:"https://blog-platform-jy0d.onrender.com" || "http://localhost:5000/api" });
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
